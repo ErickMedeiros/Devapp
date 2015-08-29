905e769f05002d72464d87a7066365cec18fc95d
Erick Medeiros <erickb_m@hotmail.com>
2015-08-29 17:44:26 -0300

    Novo APP

---
+++ Devmedia/src/controle/ControlePrincipal.java
@@ -0,0 +1,29 @@
+package controle;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "controlePrincipal")
+@RequestScoped
+
+public class ControlePrincipal implements Serializable {
+	
+	private String mensagem;
+
+	public ControlePrincipal (){
+		
+		mensagem = "Bem vindo a sua primeira aplicação com JSF2";
+		
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
