1b914a37a5b126b6b0c988cfdf1525a363532d71
Erick Medeiros <erickb_m@hotmail.com>
2015-09-07 13:03:33 -0300

    Nova atualização, Controle Tabela, Pessoae e Views

---
+++ Devmedia/src/controle/ControleTabela.java
@@ -0,0 +1,38 @@
+package controle;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+
+
+@ManagedBean (name ="controleTabela")
+@SessionScoped
+public class ControleTabela implements Serializable {
+
+	private List<Pessoa> lista;
+	
+	public ControleTabela(){
+		lista = new ArrayList<Pessoa>();
+		lista.add(new Pessoa(1, "Erick Medeiros", "87-99183-2022"));
+		lista.add(new Pessoa(2, "Romeu Medeiros", "87-98893-2022"));
+		lista.add(new Pessoa(3, "Maria Medeiros", "81-99383-2290"));
+		lista.add(new Pessoa(4, "Lilian Medeiros", "87-99183-2902"));
+	}
+	
+	
+	
+	public List<Pessoa> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Pessoa> lista) {
+		this.lista = lista;
+	}
+	
+	
+}
---
+++ Devmedia/src/controle/Pessoa.java
@@ -0,0 +1,55 @@
+package controle;
+
+import java.io.Serializable;
+
+
+
+
+public class Pessoa implements Serializable{
+
+	private Integer Id_Pessoa;
+	private String nome_Pessoa;
+	private String fone_Pessoa;
+	
+	
+	public Pessoa(){
+		
+	}
+	
+	public Pessoa(Integer Id_Pessoa, String nome_Pessoa, String fone_Pessoa){
+		
+				 this.Id_Pessoa = Id_Pessoa;
+				 this.nome_Pessoa = nome_Pessoa;
+				 this.fone_Pessoa = fone_Pessoa;
+				 
+				 
+	}
+
+	public Integer getId_Pessoa() {
+		return Id_Pessoa;
+	}
+
+	public void setId_Pessoa(Integer id_Pessoa) {
+		Id_Pessoa = id_Pessoa;
+	}
+
+	public String getNome_Pessoa() {
+		return nome_Pessoa;
+	}
+
+	public void setNome_Pessoa(String nome_Pessoa) {
+		this.nome_Pessoa = nome_Pessoa;
+	}
+
+	public String getFone_Pessoa() {
+		return fone_Pessoa;
+	}
+
+	public void setFone_Pessoa(String fone_Pessoa) {
+		this.fone_Pessoa = fone_Pessoa;
+	}
+
+}
+
+
+	
