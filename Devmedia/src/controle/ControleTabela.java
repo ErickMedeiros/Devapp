c62671c8e299d73640683bb1c90ec8a09bcbdf27
Erick Medeiros <erickb_m@hotmail.com>
2015-09-11 13:42:13 -0300

    Atualização de erro corrigido

--- Devmedia/src/controle/ControleTabela.java
+++ Devmedia/src/controle/ControleTabela.java
@@ -22,10 +22,14 @@ public class ControleTabela implements Serializable {
 		lista.add(new Pessoa(2, "Romeu Medeiros", "87-98893-2022"));
 		lista.add(new Pessoa(3, "Maria Medeiros", "81-99383-2290"));
 		lista.add(new Pessoa(4, "Lilian Medeiros", "87-99183-2902"));
+		lista.add(new Pessoa(5, "Erick Medeiros", "87-99183-2022"));
+		lista.add(new Pessoa(6, "Romeu Medeiros", "87-98893-2022"));
+		lista.add(new Pessoa(7, "Maria Medeiros", "81-99383-2290"));
+		lista.add(new Pessoa(8, "Lilian Medeiros", "87-99183-2902"));
 	}
 	
 	public String salvar(){
-		for (Pessoa obg:lista)obg.setEditando_Pessoa(false);
+		for (Pessoa obj : lista)obj.setEditando_Pessoa(false);
 		return null;
 	}
 	
