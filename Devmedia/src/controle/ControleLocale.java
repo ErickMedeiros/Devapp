e7d545f70b4f6183accf81ceef6dffe33efc777b
Erick Medeiros <Erick Medeiros@NB>
2015-10-21 12:02:56 -0300

    Nova atualização de internacionalização

---
+++ Devmedia/src/controle/ControleLocale.java
@@ -0,0 +1,12 @@
+package controle;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "controleLocale")
+@RequestScoped
+public class ControleLocale implements Serializable {
+
+}
